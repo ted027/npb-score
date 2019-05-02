8867cfec35fe19661b26171ba657b64f556319fa
ted027 <imaitt0207@gmail.com>
2019-05-02 13:15:41 +0900

    upd

--- records.py
+++ records.py
@@ -4,6 +4,21 @@ from bs4 import BeautifulSoup
 
 BASEURL = 'https://baseball.yahoo.co.jp/'
 
+NAME_HI = -1
+TEAM_H1 = -2
+
+EXCEPT_TITLE = 1
+EXCEPT_TITLE_HEADER = 2
+
+EXCEPT_HEAD_CONTENT = 1
+
+CHANCE_STR_DIVIDER = 4
+
+PITCHER_DUMP_VAL = 1
+HITTER_DUMP_VAL = 2
+
+TEAM_NUM_LIST = [376 if i == 10 else i for i in list(range(1, 13))]
+
 
 def request_soup(url):
     res = requests.get(url)
@@ -19,8 +34,8 @@ def link_tail_list(url):
 
 
 def basic_information(personal_soup):
-    name = personal_soup.find_all('h1')[-1].text.split('（')[0]
-    team = personal_soup.find_all('h1')[-2].text
+    name = personal_soup.find_all('h1')[NAME_HI].text.split('（')[0]
+    team = personal_soup.find_all('h1')[TEAM_H1].text
     return {'Name': name, 'Team': team}
 
 
@@ -59,8 +74,7 @@ def confirm_hitter_tables(tables):
 
 
 def dict_records(records_table):
-    # [1:] remove '投手/打者成績'
-    rheader = [th.text for th in records_table.find_all('th')[1:]]
+    rheader = [th.text for th in records_table.find_all('th')[EXCEPT_TITLE:]]
     rbody = [td.text for td in records_table.find_all('td')]
     return dict(zip(rheader, rbody))
 
@@ -69,40 +83,44 @@ def chance_records(chance_table):
     chheader_raw = [th.text for th in chance_table.find_all('th')]
     # [0][:4]'得点圏' + header値
     # [1:] remove table title
-    chheader = [chheader_raw[0][:4] + h for h in chheader_raw[1:]]
+    chheader = [chheader_raw[0][:CHANCE_STR_DIVIDER] + h for h in chheader_raw[EXCEPT_HEAD_CONTENT:]]
 
     chbody = [td.text for td in chance_table.find_all('td')]
     return dict(zip(chheader, chbody))
 
 
 def records_by_rl(rl_table, dump_val):
-    rl_header = [th.text for th in rl_table.find_all('th')]
+    """
+    dump_val: remove top contentof
+            pitcher: 1 ('打者')
+            hitter: 2 ('投手', '打席')
+    """
+    rl_header = [th.text for th in rl_table.find_all('th')][dump_val:]
     r_header = ['対右' + h for h in rl_header]
     l_header = ['対左' + h for h in rl_header]
 
-    rl_tr = rl_table.find_all('tr')
-    rl_body1 = [td.text for td in rl_tr[-2].find_all('td')]
-    if not '右' in rl_body1[0]:
-        raise BaseException('cannot find the word that means right records')
-    rl_records = dict(zip(r_header, rl_body1[dump_val:]))
+    rl_trs = rl_table.find_all('tr')[EXCEPT_TITLE_HEADER:]
+    rl_records = {}
+    for rl_tr in rl_trs:
+        rl_body = [td.text for td in rl_tr.find_all('td')]
+        if '右' in rl_body[0]:
+            rl_records['対右'] = dict(zip(rl_header, rl_body[dump_val:]))
+        elif '左' in rl_body[0]:
+            rl_records['対左'] = dict(zip(rl_header, rl_body[dump_val:]))
 
-    rl_body2 = [td.text for td in rl_tr[-1].find_all('td')]
-    if not '左' in rl_body2[0]:
-        raise BaseException('cannot find the word that means lenft records')
-    rl_records.update(dict(zip(l_header, rl_body2[dump_val:])))
     return rl_records
 
 
 def records_by_count_or_runner(table_by):
     # [1:] remove header content 'カウント/ランナー'
-    header = [th.text for th in table_by.find_all('th')][1:]
+    header = [th.text for th in table_by.find_all('th')][EXCEPT_HEAD_CONTENT:]
 
     # [2:] remove title and header row
-    body_tr = table_by.find_all('tr')[2:]
+    body_tr = table_by.find_all('tr')[EXCEPT_TITLE_HEADER:]
     records_by_count_or_runner = {}
     for tr in body_tr:
         body = [td.text for td in tr.find_all('td')]
-        records_by_count_or_runner[body[0]] = dict(zip(header, body[1:]))
+        records_by_count_or_runner[body[0]] = dict(zip(header, body[EXCEPT_HEAD_CONTENT:]))
     return records_by_count_or_runner
 
 
@@ -134,9 +152,10 @@ def append_team_pitcher_array(link_tail_list):
 
         records = dict_records(records_table)
 
-        # 1: dump '○打'
-        records_rl = records_by_rl(rl_table, 1)
-        records.update(records_rl)
+        if rl_table:
+            # 1: dump '○打'
+            records_rl = records_by_rl(rl_table, PITCHER_DUMP_VAL)
+            records.update(records_rl)
 
         personal_dict['Records'] = records
 
@@ -173,18 +192,22 @@ def append_team_hitter_array(link_tail_list):
 
         records = dict_records(records_table)
 
-        chance_records = chance_records(chance_table)
-        records.update(chance_records)
+        if chance_table:
+            ch_records = chance_records(chance_table)
+            records.update(ch_records)
 
-        # 2: dump '○投' | '○打席'
-        records_rl = records_by_rl(rl_table, 2)
-        records.update(records_rl)
+        if rl_table:
+            # 2: dump '○投' | '○打席'
+            records_rl = records_by_rl(rl_table, HITTER_DUMP_VAL)
+            records.update(records_rl)
 
-        records_by_count = records_by_count_or_runner(count_table)
-        records.update(records_by_count)
+        if count_table:
+            records_by_count = records_by_count_or_runner(count_table)
+            records.update({'カウント': records_by_count})
 
-        records_by_runner = records_by_count_or_runner(runner_table)
-        records.update(records_by_runner)
+        if runner_table:
+            records_by_runner = records_by_count_or_runner(runner_table)
+            records.update({'走者': records_by_runner})
 
         personal_dict['Records'] = records
 
@@ -196,7 +219,7 @@ def append_team_hitter_array(link_tail_list):
 def append_records_array():
     pitcher_list = []
     hitter_list = []
-    for i in range(1, 13):
+    for i in TEAM_NUM_LIST:
 
         purl = BASEURL + 'npb/teams/' + str(i) + '/memberlist?type=p'
         hurl = BASEURL + 'npb/teams/' + str(i) + '/memberlist?type=b'
@@ -216,7 +239,7 @@ def append_records_array():
 pitcher_list, hitter_list = append_records_array()
 
 with open('pitchers.json', 'w') as pf:
-    json.dump({'Pitcher': pitcher_list}, pf)
+    json.dump({'Pitcher': pitcher_list}, pf, indent=2, ensure_ascii=False)
 
 with open('hitters.json', 'w') as hf:
-    json.dump({'Hitter': hitter_list}, hf)
+    json.dump({'Hitter': hitter_list}, hf, indent=2, ensure_ascii=False)
