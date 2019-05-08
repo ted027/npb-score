d2b32b0b7baa8959973e2e16c61d84b79d2e3664
ted027 <imaitt0207@gmail.com>
2019-05-08 23:29:57 +0900

    fip

--- records_mod/main.py
+++ records_mod/main.py
@@ -1,9 +1,10 @@
 from records import write_y_records
-from npb import update_y_records
+from npb import update_pitcher_y_records, update_hitter_y_records
 from sabr import add_sabr_pitcher, add_sabr_hitter
 
 if __name__ == "__main__":
     write_y_records()
-    update_y_records()
+    update_pitcher_y_records()
+    update_hitter_y_records()
     add_sabr_pitcher()
     add_sabr_hitter()
--- records_mod/npb.py
+++ records_mod/npb.py
@@ -9,10 +9,10 @@ TEAM_INITIAL_LIST = ['l','h','f','b','m','e','c','s','g','db','d','t']
 INTENTIONAL_BB_COLUMN = 17
 
 
-def create_intentional_bb_dict():
+def create_intentional_bb_dict(p_or_b):
     intentional_bb_dict = {}
     for team_initial in TEAM_INITIAL_LIST:
-        url = f'http://npb.jp/bis/2019/stats/idb1_{team_initial}.html'
+        url = f'http://npb.jp/bis/2019/stats/id{p_or_b}1_{team_initial}.html'
         soup = request_soup(url)
         player_trs = soup.find_all('tr', class_='ststats')
         team_intentional_bb_dict = {pl_trs.find('td', class_='stplayer').text.replace('　', ' '): pl_trs.find_all('td')[INTENTIONAL_BB_COLUMN].text for pl_trs in player_trs}
@@ -20,18 +20,35 @@ def create_intentional_bb_dict():
     return intentional_bb_dict
 
 
-def update_y_records():
-    intentional_bb_dict = create_intentional_bb_dict()
+def update_hitter_y_records():
+    hitter_ibb_dict = create_intentional_bb_dict('b')
 
     with open('hitters.json', 'r') as hf:
         hitter_list = json.load(hf)['Hitter']
 
     for hitter in hitter_list:
-        if not hitter['Records']['試合']:
+        if not hitter['試合']:
             intentional_bb = '0'
         else:
-            intentional_bb = intentional_bb_dict.get(hitter['Name'], '0')
-        hitter['Records']['故意四球'] = intentional_bb
+            intentional_bb = hitter_ibb_dict.get(hitter['Name'], '0')
+        hitter['故意四球'] = intentional_bb
 
     with open('hitters.json', 'w') as hf:
         json.dump({'Hitter': hitter_list}, hf, indent=2, ensure_ascii=False)
+
+
+def update_pitcher_y_records():
+    pitcher_ibb_dict = create_intentional_bb_dict('p')
+
+    with open('pitchers.json', 'r') as pf:
+        pitcher_list = json.load(pf)['Pitcher']
+
+    for pitcher in pitcher_list:
+        if not pitcher['登板']:
+            intentional_bb = '0'
+        else:
+            intentional_bb = pitcher_ibb_dict.get(pitcher['Name'], '0')
+        pitcher['故意四球'] = intentional_bb
+
+    with open('pitchers.json', 'w') as pf:
+        json.dump({'Pitcher': pitcher_list}, pf, indent=2, ensure_ascii=False)
