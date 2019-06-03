a7a9a0b79574763b4978a08a1f927ae93b4cb85e
imai.tetsu <imai.tetsu@canon.co.jp>
2019-06-03 12:01:04 +0900

    rm values method

--- webui/src/components/datastore/Common.js
+++ webui/src/components/datastore/Common.js
@@ -1,50 +1,48 @@
 export function createHeader(array, regulated, order) {
-    const header = [
-      {
-        id: "名前",
-        numeric: false,
-        regulated: false,
-        disablePadding: true,
-        label: "選手"
-      }
-    ];
-    for (var i = 0; i < array.length; i++) {
-      header.push({
-        id: array[i],
-        numeric: true,
-        defaultOrder: order[i],
-        regulated: regulated[i],
-        disablePadding: true,
-        label: array[i]
-      });
+  const header = [
+    {
+      id: "名前",
+      numeric: false,
+      regulated: false,
+      disablePadding: true,
+      label: "選手"
     }
-    return header;
+  ];
+  for (var i = 0; i < array.length; i++) {
+    header.push({
+      id: array[i],
+      numeric: true,
+      defaultOrder: order[i],
+      regulated: regulated[i],
+      disablePadding: true,
+      label: array[i]
+    });
   }
-  
-  export function createBody(array, head) {
-    const body = [];
-    for (var j = 0; j < array.length; j++) {
-      var name =
-        array[j]["Name"].split(" ")[0] +
-        " (" +
-        array[j]["Team"].slice(0, 1) +
-        ")";
-      const body_player = { 名前: name };
-      for (var k = 0; k < head.length; k++) {
-        body_player[head[k]] = array[j][head[k]];
-      }
-      body.push(createData(body_player));
+  return header;
+}
+
+export function createBody(array, head) {
+  const body = [];
+  for (var j = 0; j < array.length; j++) {
+    var name =
+      array[j]["Name"].split(" ")[0] +
+      " (" +
+      array[j]["Team"].slice(0, 1) +
+      ")";
+    const body_player = { 名前: name };
+    for (var k = 0; k < head.length; k++) {
+      body_player[head[k]] = array[j][head[k]];
     }
-    return body;
+    body.push(createData(body_player));
   }
-  
-  export function createData(json) {
-    var keys = Object.keys(json);
-    var values = Object.values(json);
-    var row = {};
-    for (var k = 0; k < keys.length; k++) {
-      row[keys[k]] = values[k];
-    }
-    return row;
+  return body;
+}
+
+export function createData(json) {
+  var keys = Object.keys(json);
+  var row = {};
+  for (var k = 0; k < keys.length; k++) {
+    row[keys[k]] = json[keys[k]];
   }
-  
+  return row;
+}
