4192813895b34cecf21ff63f8de431a25657ad52
imai.tetsu <imai.tetsu@canon.co.jp>
2019-06-03 09:21:32 +0900

    upd

--- webui/src/components/Ad.js
+++ webui/src/components/Ad.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Paper from "@material-ui/core/Paper";
 
 // thin dazn
 export function top_ad() {
   return (
-    <Typography>
+    <Typography max-width="720px">
       <a
         href="https://px.a8.net/svt/ejp?a8mat=35F4ZJ+1IRWFM+491Q+686ZL"
         target="_blank"
@@ -17,6 +15,7 @@ export function top_ad() {
         <img
           border="0"
           width="100%"
+          max-width="720px"
           height="width"
           alt=""
           src="https://www24.a8.net/svt/bgt?aid=190502911092&wid=001&eno=01&mid=s00000019835001046000&mc=1"
@@ -45,6 +44,7 @@ export function middle_ad1() {
         <img
           border="0"
           width="100%"
+          max-width="720px"
           height="width"
           alt=""
           src="https://www28.a8.net/svt/bgt?aid=190503911122&wid=001&eno=01&mid=s00000019447001045000&mc=1"
@@ -62,11 +62,11 @@ export function middle_ad1() {
 }
 
 // amazon baseball games
-export function middle_ad2(styles) {
+export function middle_ad2() {
   return (
     <Toolbar style={{ margin: "auto" }}>
       <p>
-        <Card>
+        <Paper>
           <a
             target="_blank"
             href="//af.moshimo.com/af/c/click?a_id=1414800&amp;p_id=170&amp;pc_id=185&amp;pl_id=4062&amp;url=https%3A%2F%2Fwww.amazon.co.jp%2F%25E3%2582%25B3%25E3%2583%258A%25E3%2583%259F%25E3%2583%2587%25E3%2582%25B8%25E3%2582%25BF%25E3%2583%25AB%25E3%2582%25A8%25E3%2583%25B3%25E3%2582%25BF%25E3%2583%2586%25E3%2582%25A4%25E3%2583%25B3%25E3%2583%25A1%25E3%2583%25B3%25E3%2583%2588-SG_B078LHP564_US-%25E5%25AE%259F%25E6%25B3%2581%25E3%2583%2591%25E3%2583%25AF%25E3%2583%2595%25E3%2583%25AB%25E3%2583%2597%25E3%2583%25AD%25E9%2587%258E%25E7%2590%25832018-PS4%2Fdp%2FB078LHP564"
@@ -83,14 +83,14 @@ export function middle_ad2(styles) {
           <img
             src="//i.moshimo.com/af/i/impression?a_id=1414800&amp;p_id=170&amp;pc_id=185&amp;pl_id=4062"
             alt=""
-            width="15"
+            width="1"
             height="1"
             style={{ border: "0px" }}
           />
-        </Card>
+        </Paper>
       </p>
       <p>
-        <Card>
+        <Paper>
           <a
             target="_blank"
             href="//af.moshimo.com/af/c/click?a_id=1414800&amp;p_id=170&amp;pc_id=185&amp;pl_id=4062&amp;url=https%3A%2F%2Fwww.amazon.co.jp%2FMLB-Show-19-%25E8%25BC%25B8%25E5%2585%25A5%25E7%2589%2588-PS4%2Fdp%2FB07NWWTRZB"
@@ -107,14 +107,14 @@ export function middle_ad2(styles) {
           <img
             src="//i.moshimo.com/af/i/impression?a_id=1414800&amp;p_id=170&amp;pc_id=185&amp;pl_id=4062"
             alt=""
-            width="15"
+            width="1"
             height="1"
             style={{ border: "0px" }}
           />
-        </Card>
+        </Paper>
       </p>
       <p>
-        <Card>
+        <Paper>
           <a
             target="_blank"
             href="//af.moshimo.com/af/c/click?a_id=1414800&amp;p_id=170&amp;pc_id=185&amp;pl_id=4062&amp;url=https%3A%2F%2Fwww.amazon.co.jp%2F%25E3%2582%25B3%25E3%2583%258A%25E3%2583%259F%25E3%2583%2587%25E3%2582%25B8%25E3%2582%25BF%25E3%2583%25AB%25E3%2582%25A8%25E3%2583%25B3%25E3%2582%25BF%25E3%2583%2586%25E3%2582%25A4%25E3%2583%25B3%25E3%2583%25A1%25E3%2583%25B3%25E3%2583%2588-PS4-%25E3%2583%2597%25E3%2583%25AD%25E9%2587%258E%25E7%2590%2583%25E3%2582%25B9%25E3%2583%2594%25E3%2583%25AA%25E3%2583%2583%25E3%2583%25842019-%25E3%2580%2590%25E5%25BA%2597%25E8%2588%2597%25E9%2599%2590%25E5%25AE%259A%25E6%2597%25A9%25E6%259C%259F%25E8%25B3%25BC%25E5%2585%25A5%25E7%2589%25B9%25E5%2585%25B8%25E3%2580%2591%25E6%25B5%25B7%25E5%25A4%2596%25E7%25A7%25BB%25E7%25B1%258D%25E9%2581%25B8%25E6%2589%258B%25E5%2585%2588%25E8%25A1%258C%25E5%2585%25A5%25E6%2589%258BDLC-%25E9%2585%258D%25E4%25BF%25A1%2Fdp%2FB07MVD4FXS"
@@ -123,6 +123,7 @@ export function middle_ad2(styles) {
             <img
               src="https://images-fe.ssl-images-amazon.com/images/I/51Nufh5pvYL._SL160_.jpg"
               alt=""
+              height="160x"
               style={{ border: "none" }}
             />
             <br />
@@ -135,7 +136,7 @@ export function middle_ad2(styles) {
             height="1"
             style={{ border: "0px" }}
           />
-        </Card>
+        </Paper>
       </p>
     </Toolbar>
   );
