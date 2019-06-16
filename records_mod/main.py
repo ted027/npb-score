817f742ac0828e3f8283214a8c83caf1d89e19e7
ted027 <imaitt0207@gmail.com>
2019-06-16 16:27:59 +0900

    update records by make 2019年  6月 16日 日曜日 16:27:59 JST

--- records_mod/main.py
+++ records_mod/main.py
@@ -6,7 +6,7 @@ from add_sabr import add_sabr_pitcher, add_sabr_hitter
 from team_by_park import update_team_park_records
 
 if __name__ == "__main__":
-    write_y_records()
+    # write_y_records()
     write_team_records()
     update_records_by_official()
     update_team_park_records()
