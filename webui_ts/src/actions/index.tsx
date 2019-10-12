9ea38754075fa9e9392d0d7387db4d025a908978
ted027 <imaitt0207@gmail.com>
2019-10-12 23:29:00 +0900

    actions to ts

--- webui_ts/src/actions/index.tsx
+++ webui_ts/src/actions/index.tsx
@@ -1,22 +1,24 @@
+import React from 'react';
+
 // const ALL = 0;
 const CENTRAL = 1;
 const PACIFIC = 2;
 
-export const changeTab = (selected: number): {[key: string]: any}  => {
+export const changeTab = (selected: number): {[key: string]: string | number}  => {
   return {
     type: "CHANGE_TAB",
     selected: selected
   };
 };
 
-export const changeOrderSelected = (order_selected: number): {[key: string]: any} => {
+export const changeOrderSelected = (order_selected: number): {[key: string]: string | number} => {
   return {
     type: "CHANGE_ORDER_SELECTED",
     order_selected: order_selected
   };
 };
 
-export const changeLeague = (league_selected: number): {[key: string]: any} => {
+export const changeLeague = (league_selected: number): {[key: string]: string | number} => {
   // if (league_selected === ALL)
   var league: string = "CentralPacific";
   if (league_selected === CENTRAL) {
@@ -47,29 +49,29 @@ export const resetSearch = (): {[key: string]: string} => {
   };
 };
 
-export const handlePopper = (event: any): {[key: string]: any} => {
-  const { currentTarget } = event;
+export const handlePopper = (event: React.MouseEvent<SVGSVGElement, MouseEvent>): {[key: string]: any} => {
+  const currentTarget = event;
   return {
     type: "HANDLE_POPPER",
     anchorEl: currentTarget,
   };
 };
 
-// export const clickAway = () => {
+// export const clickAway = (): {[key: string]: string | boolean}  => {
 //     return {
 //         type: 'CLICK_AWAY',
 //         open: false
 //     }
 // }
 
-export const decideTeamText = event => {
+export const decideTeamText = (event: any): {[key: string]: string} => {
   return {
     type: "DECIDE_TEAM_TEXT",
     team: event.target.value
   };
 };
 
-export const decideNameText = event => {
+export const decideNameText = (event: any): {[key: string]: string} => {
   return {
     type: "DECIDE_NAME_TEXT",
     name: event.target.value
