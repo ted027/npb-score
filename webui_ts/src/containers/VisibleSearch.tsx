bfdd85fc3914d112358e8406b3857a3f9233313d
ted027 <imaitt0207@gmail.com>
2019-10-15 00:11:34 +0900

    wip reducer

---
+++ webui_ts/src/containers/VisibleSearch.tsx
@@ -0,0 +1,46 @@
+import { connect } from "react-redux";
+import { Dispatch } from 'redux';
+import Search from "../components/Search";
+import {
+  execSearch,
+  resetSearch,
+  handlePopper,
+  decideTeamText,
+  decideNameText
+} from "../actions";
+
+const mapStateToProps = state => {
+  return { searchState: state.Search };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    execSearch: (team: string, name: string) => {
+      dispatch(execSearch(team, name));
+    },
+    resetSearch: (event: any) => {
+      dispatch(resetSearch());
+    },
+    handlePopper: (event: any) => {
+      dispatch(handlePopper(event));
+    },
+    // clickAway: (event: any) => {
+    //     dispatch(clickAway())
+    // },
+    decideTeamText: (event: any) => {
+      dispatch(decideTeamText(event));
+    },
+    decideNameText: (event: any) => {
+      dispatch(decideNameText(event));
+    }
+  };
+};
+
+const VisibleSearch = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  null,
+  { forwardRef: true }
+)(Search);
+
+export default VisibleSearch;
---
+++ webui_ts/src/containers/changeTab.tsx
@@ -0,0 +1,53 @@
+import { connect } from "react-redux";
+import { Dispatch } from 'redux';
+import { MainAppBar, OrderAppBar, LeagueAppBar } from "../components/Pages";
+import { changeTab, changeOrderSelected, changeLeague } from "../actions";
+
+const mapStateToProps = () => {
+  return {};
+};
+
+const mapMainDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    onChange: (event: any, selected: number) => {
+      dispatch(changeTab(selected));
+    }
+  };
+};
+
+const mapOrderDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    onChange: (event: any, order_selected: number) => {
+      dispatch(changeOrderSelected(order_selected));
+    }
+  };
+};
+
+const mapLeagueDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    onChange: (event: any, league_selected: number) => {
+      dispatch(changeLeague(league_selected));
+    }
+  };
+};
+
+export const VisibleMainAppBar = connect(
+  mapStateToProps,
+  mapMainDispatchToProps,
+  null,
+  { forwardRef: true }
+)(MainAppBar);
+
+export const VisibleOrderAppBar = connect(
+  mapStateToProps,
+  mapOrderDispatchToProps,
+  null,
+  { forwardRef: true }
+)(OrderAppBar);
+
+export const VisibleLeagueAppBar = connect(
+  mapStateToProps,
+  mapLeagueDispatchToProps,
+  null,
+  { forwardRef: true }
+)(LeagueAppBar);
