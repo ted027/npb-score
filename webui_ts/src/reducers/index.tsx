cb4b126b67a77219d3aac5db9f6c0f57fef441e5
ted027 <imaitt0207@gmail.com>
2019-12-21 22:54:59 +0900

    typescript state type

--- webui_ts/src/reducers/index.tsx
+++ webui_ts/src/reducers/index.tsx
@@ -1,6 +1,11 @@
 import { combineReducers } from "redux";
-import MainPage from "./page";
-import Search from "./search";
+import MainPage, { MainPageIf } from "./page";
+import Search, { SearchIf } from "./search";
+
+export interface state {
+    MainPage: MainPageIf,
+    Search: SearchIf
+}
 
 const recordsApp = combineReducers({ MainPage, Search });
 export default recordsApp;
