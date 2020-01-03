2b9d6745df50fb1f515a0d1a5b8b91571ee50993
ted027 <imaitt0207@gmail.com>
2020-01-03 16:01:59 +0900

    wip multi years

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 
+YEAR="2019"
+
 cd records_mod
 python main.py
 cd -
-cp -rf records_mod/records/2019 webui/src/2019
+cp -rf records_mod/records/$YEAR webui/src/$YEAR
 
 git add --all
 msg="update records by make `date`"
