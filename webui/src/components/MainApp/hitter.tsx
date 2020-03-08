7cb8ac771dcc47eb4552dfb81a9fffdb6b8c903f
ted027 <imaitt0207@gmail.com>
2020-03-08 22:42:00 +0900

    mainappをreact.fcに分割

---
+++ webui/src/components/MainApp/hitter.tsx
@@ -0,0 +1,235 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { HideOnScroll } from "../Pages";
+import { CommonTable } from "../Tables";
+import { VisibleLeagueAppBar } from "../../containers/changeTab";
+import { VisibleSearch } from "../../containers/VisibleSearch";
+import { top_ad, middle_ad1, middle_ad2, middle_ad3, middle_ad4 } from "../Ad";
+import { MainProps } from ".";
+import {
+  hitters_sabr_header,
+  hitters_header_title,
+  hitters_header_ops,
+  hitters_header_woba,
+  hitters_header_xr,
+  hitters_header_contact,
+  hitters_header_power,
+  hitters_header_eye,
+  hitters_header_steal,
+  hitters_header_clutch,
+  hitters_header_oth,
+  hitters_body_of_year
+} from "../../datastore/Hitters";
+
+export const Hitter: React.FC<MainProps> = props => {
+  const { classes, pageState, yearState } = props;
+  const { league_selected, league } = pageState;
+  const { year_selected } = yearState;
+  return (
+    <div className={classes.individualRoot}>
+      <HideOnScroll {...props} direction="down">
+        <VisibleLeagueAppBar selected={league_selected} />
+      </HideOnScroll>
+      <div className={classes.fab}>
+        <HideOnScroll {...props} direction="up">
+          <VisibleSearch />
+        </HideOnScroll>
+      </div>
+      {top_ad(classes)}
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              野手総合指標
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="wRC+"
+          head={hitters_sabr_header}
+          data={hitters_body_of_year(year_selected).sabr}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              野手タイトル
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="打率"
+          head={hitters_header_title}
+          data={hitters_body_of_year(year_selected).title}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      {middle_ad2(classes)}
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              打撃力総合１
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="出塁率"
+          head={hitters_header_ops}
+          data={hitters_body_of_year(year_selected).ops}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              打撃力総合２
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="wOBA"
+          head={hitters_header_woba}
+          data={hitters_body_of_year(year_selected).woba}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      {middle_ad1(classes)}
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              攻撃力総合
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="XR"
+          head={hitters_header_xr}
+          data={hitters_body_of_year(year_selected).xr}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              ミート
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="asc"
+          default_orderBy="K%"
+          head={hitters_header_contact}
+          data={hitters_body_of_year(year_selected).contact}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      {middle_ad4(classes)}
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              パワー
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="IsoP"
+          head={hitters_header_power}
+          data={hitters_body_of_year(year_selected).power}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              選球眼
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="BB%"
+          head={hitters_header_eye}
+          data={hitters_body_of_year(year_selected).eye}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      {middle_ad2(classes)}
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              走力
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="wSB"
+          head={hitters_header_steal}
+          data={hitters_body_of_year(year_selected).steal}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              チャンス
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="圏打率"
+          head={hitters_header_clutch}
+          data={hitters_body_of_year(year_selected).clutch}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+      {middle_ad3(classes)}
+      <div>
+        <AppBar position="static" color="default" className={classes.des}>
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.des2}>
+              その他
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <CommonTable
+          default_order="desc"
+          default_orderBy="BABIP"
+          head={hitters_header_oth}
+          data={hitters_body_of_year(year_selected).oth}
+          league={league}
+          main_state={pageState}
+        />
+      </div>
+    </div>
+  );
+};
--- webui/src/components/MainApp/order.tsx
+++ webui/src/components/MainApp/order.tsx
@@ -6,6 +6,7 @@ import { HideOnScroll } from "../Pages";
 import { CommonTable } from "../Tables";
 import { VisibleOrderAppBar } from "../../containers/changeTab";
 import { top_ad, middle_ad1, middle_ad4 } from "../Ad";
+import { MainProps } from ".";
 import {
   teams_header_stats,
   teams_header_offense,
@@ -21,210 +22,179 @@ import {
 const ORDER = 0;
 const PARKFACTOR = 1;
 
-export const Order = (props: any) => {
-  <div className={props.classes.individualRoot}>
-    <HideOnScroll {...props} direction="down">
-      <VisibleOrderAppBar selected={props.pageState.order_selected} />
-    </HideOnScroll>
-    {top_ad(props.classes)}
-    {props.pageState.order_selected === ORDER && (
-      <div>
+export const Order: React.FC<MainProps> = (props) => {
+  const { classes, pageState, yearState } = props;
+  const { order_selected } = pageState;
+  const { year_selected } = yearState;
+  return (
+    <div className={classes.individualRoot}>
+      <HideOnScroll {...props} direction="down">
+        <VisibleOrderAppBar selected={order_selected} />
+      </HideOnScroll>
+      {top_ad(classes)}
+      {order_selected === ORDER && (
         <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                セリーグ順位表
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="desc"
-            default_orderBy="勝率"
-            head={teams_header_stats}
-            data={teams_body_of_year(props.yearState.year_selected).stats}
-            const_row_length={
-              teams_body_of_year(props.yearState.year_selected).stats.length
-            }
-            league="Central"
-            main_state={props.pageState}
-          />
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  セリーグ順位表
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="desc"
+              default_orderBy="勝率"
+              head={teams_header_stats}
+              data={teams_body_of_year(year_selected).stats}
+              const_row_length={teams_body_of_year(year_selected).stats.length}
+              league="Central"
+              main_state={pageState}
+            />
+          </div>
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  パリーグ順位表
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="desc"
+              default_orderBy="勝率"
+              head={teams_header_stats}
+              data={teams_body_of_year(year_selected).stats}
+              const_row_length={teams_body_of_year(year_selected).stats.length}
+              league="Pacific"
+              main_state={pageState}
+            />
+          </div>
+          {middle_ad1(classes)}
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  セリーグ野手成績
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="desc"
+              default_orderBy="得点"
+              head={teams_header_offense}
+              data={teams_body_of_year(year_selected).offense}
+              const_row_length={
+                teams_body_of_year(year_selected).offense.length
+              }
+              league="Central"
+              main_state={pageState}
+            />
+          </div>
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  パリーグ野手成績
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="desc"
+              default_orderBy="得点"
+              head={teams_header_offense}
+              data={teams_body_of_year(year_selected).offense}
+              const_row_length={
+                teams_body_of_year(year_selected).offense.length
+              }
+              league="Pacific"
+              main_state={pageState}
+            />
+          </div>
+          {middle_ad4(classes)}
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  セリーグ投手成績
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="asc"
+              default_orderBy="失点"
+              head={teams_header_defense}
+              data={teams_body_of_year(year_selected).defense}
+              const_row_length={
+                teams_body_of_year(year_selected).defense.length
+              }
+              league="Central"
+              main_state={pageState}
+            />
+          </div>
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  パリーグ投手成績
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="asc"
+              default_orderBy="失点"
+              head={teams_header_defense}
+              data={teams_body_of_year(year_selected).defense}
+              const_row_length={
+                teams_body_of_year(year_selected).defense.length
+              }
+              league="Pacific"
+              main_state={pageState}
+            />
+          </div>
         </div>
+      )}
+      {order_selected === PARKFACTOR && (
         <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                パリーグ順位表
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="desc"
-            default_orderBy="勝率"
-            head={teams_header_stats}
-            data={teams_body_of_year(props.yearState.year_selected).stats}
-            const_row_length={
-              teams_body_of_year(props.yearState.year_selected).stats.length
-            }
-            league="Pacific"
-            main_state={props.pageState}
-          />
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  パークファクター(2019)　※参考値
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="desc"
+              default_orderBy="得点PF"
+              head={parks_header}
+              data={parks_body}
+              const_row_length={parks_body.length}
+              league=""
+              main_state={pageState}
+            />
+          </div>
+          {middle_ad1(classes)}
+          <div>
+            <AppBar position="static" color="default" className={classes.des}>
+              <Toolbar variant="dense">
+                <Typography variant="h6" className={classes.des2}>
+                  パークファクター(2016以降)　※参考値
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <CommonTable
+              default_order="desc"
+              default_orderBy="得点PF"
+              head={parks_header}
+              data={parks_total_body}
+              const_row_length={parks_body.length}
+              league=""
+              main_state={pageState}
+            />
+          </div>
         </div>
-        {middle_ad1(props.classes)}
-        <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                セリーグ野手成績
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="desc"
-            default_orderBy="得点"
-            head={teams_header_offense}
-            data={teams_body_of_year(props.yearState.year_selected).offense}
-            const_row_length={
-              teams_body_of_year(props.yearState.year_selected).offense.length
-            }
-            league="Central"
-            main_state={props.pageState}
-          />
-        </div>
-        <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                パリーグ野手成績
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="desc"
-            default_orderBy="得点"
-            head={teams_header_offense}
-            data={teams_body_of_year(props.yearState.year_selected).offense}
-            const_row_length={
-              teams_body_of_year(props.yearState.year_selected).offense.length
-            }
-            league="Pacific"
-            main_state={props.pageState}
-          />
-        </div>
-        {middle_ad4(props.classes)}
-        <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                セリーグ投手成績
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="asc"
-            default_orderBy="失点"
-            head={teams_header_defense}
-            data={teams_body_of_year(props.yearState.year_selected).defense}
-            const_row_length={
-              teams_body_of_year(props.yearState.year_selected).defense.length
-            }
-            league="Central"
-            main_state={props.pageState}
-          />
-        </div>
-        <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                パリーグ投手成績
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="asc"
-            default_orderBy="失点"
-            head={teams_header_defense}
-            data={teams_body_of_year(props.yearState.year_selected).defense}
-            const_row_length={
-              teams_body_of_year(props.yearState.year_selected).defense.length
-            }
-            league="Pacific"
-            main_state={props.pageState}
-          />
-        </div>
-      </div>
-    )}
-    {props.pageState.order_selected === PARKFACTOR && (
-      <div>
-        <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                パークファクター(2019)　※参考値
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="desc"
-            default_orderBy="得点PF"
-            head={parks_header}
-            data={parks_body}
-            const_row_length={parks_body.length}
-            league=""
-            main_state={props.pageState}
-          />
-        </div>
-        {middle_ad1(props.classes)}
-        <div>
-          <AppBar
-            position="static"
-            color="default"
-            className={props.classes.des}
-          >
-            <Toolbar variant="dense">
-              <Typography variant="h6" className={props.classes.des2}>
-                パークファクター(2016以降)　※参考値
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CommonTable
-            default_order="desc"
-            default_orderBy="得点PF"
-            head={parks_header}
-            data={parks_total_body}
-            const_row_length={parks_body.length}
-            league=""
-            main_state={props.pageState}
-          />
-        </div>
-      </div>
-    )}
-  </div>;
+      )}
+    </div>
+  );
 };
