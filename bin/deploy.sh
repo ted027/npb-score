cadc513640aa5c46027cbac5cb30e4eb63df6d66
imai.tetsu <imai.tetsu@canon.co.jp>
2020-07-15 14:19:13 +0900

    fix copy job again

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ YEAR="2020"
 cd records_mod
 python main.py
 cd -
-cp -rf records_mod/records/$YEAR webui/src/records/$YEAR
+cp -rf records_mod/records/$YEAR/* webui/src/records/$YEAR
 
 git add --all
 msg="update records by make `date`"
