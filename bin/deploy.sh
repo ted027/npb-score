30874a1dc0c22d954d6effec2646853e94c500b3
ted027 <imaitt0207@gmail.com>
2020-07-18 22:14:36 +0900

    manual update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,7 @@ cd records_mod
 python main.py
 cd -
 cp -rf records_mod/records/$YEAR/* webui/src/records/$YEAR
+cp -rf webui/src/records/$YEAR/parks.json webui/src/records
 
 git add --all
 msg="update records by make `date`"
--- records_mod/main.py
+++ records_mod/main.py
@@ -8,22 +8,24 @@ from team_by_park import update_team_park_records
 from multiprocessing import Process
 
 if __name__ == "__main__":
-    # pitch_process = Process(target=write_pitcher_records)
-    # pitch_process.start()
+    # parallel
+    pitch_process = Process(target=write_pitcher_records)
+    pitch_process.start()
 
-    # hit_process = Process(target=write_hitter_records)
-    # hit_process.start()
+    hit_process = Process(target=write_hitter_records)
+    hit_process.start()
 
-    # team_process = Process(target=write_team_records)
-    # team_process.start()
+    team_process = Process(target=write_team_records)
+    team_process.start()
 
-    # pitch_process.join()
-    # hit_process.join()
-    # team_process.join()
+    pitch_process.join()
+    hit_process.join()
+    team_process.join()
 
-    write_team_records()
-    write_hitter_records()
-    write_pitcher_records()
+    # not parallel
+    # write_team_records()
+    # write_hitter_records()
+    # write_pitcher_records()
 
     update_records_by_official()
 
